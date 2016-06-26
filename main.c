275b7880c18b3bfe2f77bda509d3328c6f0e96b2
mavocado3 <gustavopetruzzi19@gmail.com>
2016-06-26 20:24:16 -0300

    testeando git branch agrego modificar numero
    testeo git agrego modificar numero

--- main.c
+++ main.c
@@ -4,9 +4,10 @@
 int main()
 {
     int numeros[5];
-    int i;
+    int i, indice;
     int flag = 1;
     int maximo,minimo;
+    char opcion = 's';
     for(i=0;i<5;i++)
     {
         printf("Ingrese un numero: ");
@@ -25,8 +26,43 @@ int main()
         {
             minimo = numeros[i];
         }
+    }
+
+    printf("MAXIMO %d  MINIMO %d\n", maximo, minimo);
+
+    while(opcion=='s')
+    {
+        printf("Desea modificar un numero? s/n\n");
+        scanf(" %c", &opcion);
+        if(opcion == 's')
+        {
+            printf("Ingrese el indice: ");
+            scanf("%d", &indice);
+
+            printf("Ingrese el nuevo numero: ");
+            scanf("%d", &numeros[indice]);
+        }
+        flag = 1;
 
     }
-    printf("MAXIMO %d  MINIMO %d", maximo, minimo);
+
+    for(i=0;i<5;i++)
+    {
+        if(flag== 1)
+        {
+            maximo = numeros[i];
+            minimo = numeros[i];
+            flag = 0;
+        }
+        if(numeros[i]> maximo)
+        {
+            maximo = numeros[i];
+        }
+        if(numeros[i]< minimo)
+        {
+            minimo = numeros[i];
+        }
+    }
+    printf("MAXIMO %d  MINIMO %d\n", maximo, minimo);
     return 0;
 }
